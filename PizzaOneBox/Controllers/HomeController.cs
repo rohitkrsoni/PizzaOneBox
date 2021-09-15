e65f3e95d07b3dfc593c62e2dfaef1eb6498113d
rohitkrsoni <60655155+rohitkrsoni@users.noreply.github.com>
2021-09-15 18:06:47 +0530

    Add project files.

---
+++ PizzaOneBox/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzaOneBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaOneBox.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+    }
+}
---
+++ PizzaOneBox/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaOneBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
