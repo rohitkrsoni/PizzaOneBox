fa27168d9fc594105bb7a93bf2752c83bf14e58d
Avanish Aeron <avanish.aeron@gmail.com>
2021-10-14 18:41:43 +0530

    Done some changes on homepage.

--- PizzaOneBox/Controllers/HomeController.cs
+++ PizzaOneBox/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace PizzaOneBox.Controllers
         {
             var pizzaList = new List<HomePagePizzaModel>()
             {
-                new HomePagePizzaModel(){Id=1, PhotoPath="/Pizza_images/margherita.jpg",PizzaDescription="A hugely popular margherita, with a deliciously tangy single cheese topping",PizzaName="Margherita",PizzaCost=14.49m},
+                new HomePagePizzaModel(){Id=1, PhotoPath="/Pizza_images/margherita.jpg",PizzaDescription="A hugely popular margherita, with a single cheese topping",PizzaName="Margherita",PizzaCost=14.49m},
                 new HomePagePizzaModel(){Id=2, PhotoPath="/Pizza_images/Farmhouse.jpg",PizzaDescription="Onion, Capsicum, Tomato, Grilled Mushroom",PizzaName="Farm House",PizzaCost=15.49m},
                 new HomePagePizzaModel(){Id=3, PhotoPath="/Pizza_images/Peppy_Paneer.jpg",PizzaDescription="Paneer, Crisp Capsicum, Red Paprika",PizzaName="Peppy Paneer",PizzaCost=15.49m},
                 new HomePagePizzaModel(){Id=4, PhotoPath="/Pizza_images/Corn_&_Cheese.jpg",PizzaDescription="Golden Corn",PizzaName="Cheese Corn",PizzaCost=14.49m},
