b4248fc8bdf205af6526ef7e23e418fd57e5d791
rohitkrsoni <60655155+rohitkrsoni@users.noreply.github.com>
2021-10-20 12:28:48 +0530

    Deleted Unwanted Files, Lines and inline CSS to site.css

--- PizzaOneBox/Controllers/ConfirmationPgController.cs
+++ PizzaOneBox/Controllers/ConfirmationPgController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PizzaOneBox.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 
 namespace PizzaOneBox.Controllers
